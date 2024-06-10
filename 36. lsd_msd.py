30a8d12d70a92153b8801c63913948c632388caf
sohithpydev <123935057+sohithpydev@users.noreply.github.com>
2024-06-10 14:34:52 +0530

    Update and rename lsd_msd.py to 36. lsd_msd.py

--- 36. lsd_msd.py
+++ 36. lsd_msd.py
@@ -1,12 +1,6 @@
-# Given number
 number = int(input("enter a number:"))
-# Convert number to string to easily access digits
 number_str = str(number)
-
-# Least Significant Digit (LSD) is the last digit
 lsd = number_str[-1]
-
-# Most Significant Digit (MSD) is the first digit
 msd = number_str[0]
 
 print("Least Significant Digit (LSD):", lsd)
