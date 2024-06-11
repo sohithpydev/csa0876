e1e578d2677a28be6cf928b624dc7cfd8a3f87e8
sohithpydev <123935057+sohithpydev@users.noreply.github.com>
2024-06-11 22:18:41 +0530

    Create 42. 3d_surface_plot.py

---
+++ 42. 3d_surface_plot.py
@@ -0,0 +1,12 @@
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+x, y = np.meshgrid(x, y)
+z = np.sin(np.sqrt(x**2 + y**2))
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(x, y, z)
+
+plt.title('3D Surface Plot Example')
+
+plt.show()
